cf414373e7019b2b1ff1cf5736dcf5e26f4c6f96
Hendri99-ml <hendriakbar294@gmail.com>
2022-06-02 17:49:18 +0800

    final-project

--- add.js
+++ add.js
@@ -13,7 +13,7 @@ function equation() {
     let allAnswer = []
     let switchAnswer = []
 
-    answer = num1 - num2
+    answer = num1 + num2
 
     document.getElementById("num1").innerHTML = num1
     document.getElementById("num2").innerHTML = num2
--- divide.js
+++ divide.js
@@ -1,46 +1,60 @@
 const option1 = document.getElementById("option1");
 const option2 = document.getElementById("option2");
 const option3 = document.getElementById("option3");
-const audio = document.getElementById("myAudio");
+const audio1 = document.getElementById("audioTrue");
+const audio2 = document.getElementById("audioFalse");
 let answer = 0;
 
 function equation() {
-    let num1 = Math.floor(Math.random() * 11)
-    let num2 = Math.floor(Math.random() * 11)
-    let answer1 = Math.floor(Math.random() * 11)
-    let answer2 = Math.floor(Math.random() * 11)
+    let num1 = Math.ceil(Math.random() * 10)
+    let num2 = Math.ceil(Math.random() * 10)
+    let answer1 = Math.ceil(Math.random() * 10)
+    let answer2 = Math.ceil(Math.random() * 10)
+    let allAnswer = []
+    let switchAnswer = []
 
-    answer = num1 / num2
+    answer = Math.floor(num1 / num2)
 
     document.getElementById("num1").innerHTML = num1
     document.getElementById("num2").innerHTML = num2
 
-    option1.innerHTML = answer
-    option2.innerHTML = answer1
-    option3.innerHTML = answer2
+    allAnswer = [answer, answer1, answer2]
+        // console.log(allAnswer);
+
+    for (let i = allAnswer.length; i > 0; i--) {
+        switchAnswer.push(allAnswer.splice(Math.floor(Math.random() * i), 1)[0])
+    }
+    // console.log(switchAnswer);
+
+    option1.innerHTML = switchAnswer[0].toFixed(0)
+    option2.innerHTML = switchAnswer[1].toFixed(0)
+    option3.innerHTML = switchAnswer[2].toFixed(0)
 }
 
 option1.addEventListener("click", function() {
     if (option1.innerHTML == answer) {
+        audio1.play()
         equation()
     } else {
-        audio.play()
+        audio2.play()
     }
 });
 
 option2.addEventListener("click", function() {
     if (option2.innerHTML == answer) {
+        audio1.play()
         equation()
     } else {
-        audio.play()
+        audio2.play()
     }
 });
 
 option3.addEventListener("click", function() {
     if (option3.innerHTML == answer) {
+        audio1.play()
         equation()
     } else {
-        audio.play()
+        audio2.play()
     }
 });
 
